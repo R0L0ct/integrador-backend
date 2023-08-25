1cae3405f6be57db3fbc74209477b14100503133
R0L0ct <103770217+R0L0ct@users.noreply.github.com>
2023-08-25 17:31:36 -0300

    fix origin cors link

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ const app = express();
 app.use(
   cors({
     // origin: "http://localhost:3000",
-    origin: "https://integrador-react-plum.vercel.app/",
+    origin: "https://integrador-react-plum.vercel.app",
     credentials: true,
   })
 );
